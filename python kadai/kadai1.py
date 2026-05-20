46d68f7d779ef239985735c256b1b201b87f191c
y251120Vishal <y251120@yamaguchi-jc.ac.jp>
2026-05-20 02:29:47 +0000

    kadai1.py

---
+++ python kadai/kadai1.py
@@ -0,0 +1,9 @@
+name = input("enter your name:")
+score = int(input("enter your score:)"))
+print("hello",name)
+if score >=60:
+    print("pass")
+    print("congrats")
+else:
+    print("fail")
+    print("try again")
