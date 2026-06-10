1400bfbf173625352391671897d68c0459a95633
y251120Vishal <y251120@yamaguchi-jc.ac.jp>
2026-06-10 02:13:20 +0000

    app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st 
+st.title("my profile")
+name = st.text_input("name")
+country = st.text_input("country")
+hobby = st.text.input("hobby")
+if name:
+    st.write(f"my name is{name}")
+if country:
+    st.write(f"i am from{country}")
+if hobby:
+    st.write(f"my hobby is{hobby}")
+    
