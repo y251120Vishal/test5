5293efd4f32208915affb121c1afafe52cc6c92c
y251120Vishal <y251120@yamaguchi-jc.ac.jp>
2026-06-10 02:15:13 +0000

    app.py

--- app.py
+++ app.py
@@ -2,11 +2,10 @@ import streamlit as st
 st.title("my profile")
 name = st.text_input("name")
 country = st.text_input("country")
-hobby = st.text.input("hobby")
+hobby = st.text_input("hobby")
 if name:
     st.write(f"my name is{name}")
 if country:
     st.write(f"i am from{country}")
 if hobby:
     st.write(f"my hobby is{hobby}")
-    
